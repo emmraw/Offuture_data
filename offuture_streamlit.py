0f454e3f9cf43b0b436fbe2a737b95d1fba7b70f
emmraw <101661775+emmraw@users.noreply.github.com>
2022-03-22 11:07:07 +0000

    Create offuture_streamlit.py

---
+++ offuture_streamlit.py
@@ -0,0 +1,10 @@
+import strealit as st
+import matplotlib as plt
+import pandas as pd
+import numpy as np
+
+st.title('Offuture Graphs')
+
+graph = df.groupby(df['Order Date'].dt.month)['Sales'].sum()
+st.dataframe(graph)
+st.line_chart(graph)
